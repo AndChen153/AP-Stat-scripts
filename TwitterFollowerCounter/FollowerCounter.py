c6b9dd131549cda758a019833e8970350b1aa3bd
andrew <achen1192@gmail.com>
2020-12-12 23:07:50 -0600

    finished followercounter

--- TwitterFollowerCounter/FollowerCounter.py
+++ TwitterFollowerCounter/FollowerCounter.py
@@ -1,6 +1,7 @@
 #https://www.geeksforgeeks.org/python-status-object-in-tweepy/
 #1-9-90 rule
 import tweepy
+import time
 
 class TwitterClient(object): 
 
@@ -9,6 +10,7 @@ class TwitterClient(object):
         CONSUMER_SECRET_KEY = "onCURThXUu4Nh3LsVreji43z3yB2DRIk3ZX09nDdfAzk4VzdYk"
         ACCESS_TOKEN = "764173298032717825-HR6jsuRav5UnCrXxjFgj6YhsHb51x8i"
         ACCESS_TOKEN_SECRET = "fIhDmUXC0vMbkUGCrTVgzgk0gKmygXSSNqYlSIfAjuj8Q"
+        self.tweetsList = []
         try:
             self.auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
             self.auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -18,9 +20,8 @@ class TwitterClient(object):
             print("Authentication Failed")
 
     def tweetSearch(self, hashtag):
-        tweets = self.api.search(q=hashtag, 
+        return self.api.search(q=hashtag, 
                     lang="en")
-        return tweets
     
     def tweetSearch30(self, hashtag, date):
         #date in format yyyyMMddHHmm
@@ -28,25 +29,62 @@ class TwitterClient(object):
         return tweets
 
     def getUser(self, tweet):
-        userList = []
         if tweet.user.location != None:
             if "Memphis" in tweet.user.location:
                 return(tweet.user.screen_name, tweet.user.followers_count)
 
+    def tweetWrite(self, hashtags):
+        noEntries = True
+        for i in hashtags:
+            tweetsList = self.tweetSearch(i)
+            self.tweetsList += tweetsList
+        
+        for i in self.tweetsList:
+            if self.getUser(i) != None:
+                write = True
+                readUsers = open("Usernames.txt", "r")
+                for x in readUsers:
+                    if str(self.getUser(i)[0]) in x:
+                        write = False
+                readUsers.close()
+
+                if write:
+                    noEntries = False
+                    print(i.user.screen_name, i.user.followers_count)
+                    writeUsers = open("Usernames.txt", "a")
+                    writeUsers.write(str(self.getUser(i)[0]) + "\n")
+                    writeUsers.close()
+                    writeFollowers = open("FollowerCts.txt", "a")
+                    writeFollowers.write(str(self.getUser(i)[1]) + "\n")
+                    writeFollowers.close()
+        if noEntries:
+            print("no new entries")
+                    
+
 search = TwitterClient()
+keywords = ["#memphis", "memphis", "Tennessee", "TN"]
+
+while True:
+    print("SEARCHING... \n")
+    search.tweetWrite(keywords)
+    time.sleep(60)
 
 
-date = 202011300000
-tweets30 = []
+
+
+
+
+
+
+for i in range(11): 
+    date -= 10000
+
+for i in range(17):
+    date2 -= 10000
+
+#print (date)
+
+
+
 '''"Grizz", "#elvis", "#graceland", "#ilovememphis", 
          "memphistennessee", "#901", "#choose901", "#memphisgrizzlies"'''
-
-for i in ("#memphis", "memphis", "Tennessee", "TN"):
-    tweetsList = search.tweetSearch30(i, date)
-    tweets30 += tweetsList
-
-for i in tweets30:
-    if search.getUser(i) != None:
-        print(search.getUser(i)[0], search.getUser(i)[1])
-print(len(tweets30))
-
